9dc9772887930b83570fdcd271c596a673a3ce92
BrianReisman <brianreisman@protonmail.com>
2021-03-05 09:41:44 -0500

    exploring explicit Types with TypeScript

--- sandbox.js
+++ sandbox.js
@@ -6,64 +6,105 @@
 //* don't worry about error after converting since variables seem to be double declared
 //*everytime you make a change in the .ts file you need to rerun tsc sandbox.ts
 //*... unless you run the command $ tsx sandbox.ts -w (w is for watch)
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var log = console.log;
-var character = "Brian";
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = "Ron";
-// age = 'thirty'
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    // const circ = (diameter: string) => {
-    // const circ = (diameter: boolean) => {
-    return diameter * Math.PI;
-};
-console.log(circ(7.5));
-//*arrays
-var names = ["Brian", "Ron", "Robin", "Steven"];
-names.push("Bubby");
-// names.push(4);
-// names.push(true);
-// names[0] = false;
-var numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('Hello');
-// let me = 'Brian';
-// numbers.push(me)
-var mixed = ["Ken", 4, "string", 8, 9];
-mixed.push(44);
-mixed.push("Text");
-// mixed.push(false)
-// mixed.push(true)
-mixed[0] = 0;
-mixed[1] = "string";
-//* objects. Property types become fixed
-var ninja = {
-    name: "Brian",
-    belt: "black",
-    age: 30
-};
-ninja.age = 40;
-ninja.name = "Test";
-// ninja.age = "30";
-// ninja.skills = ['fight', 'rest', 'sneaking']; //*new property cannot be added to existing object
-ninja = __assign({}, ninja);
-ninja = {
-    name: "Patrick",
-    belt: "blue",
-    age: 32
-};
+// const log = console.log;
+// let character = "Brian";
+// let age = 30;
+// let isBlackBelt = false;
+// // character = 20;
+// character = "Ron";
+// // age = 'thirty'
+// age = 40;
+// // isBlackBelt = 'yes';
+// isBlackBelt = true;
+// const circ = (diameter: number) => {
+//   // const circ = (diameter: string) => {
+//   // const circ = (diameter: boolean) => {
+//   return diameter * Math.PI;
+// };
+// // console.log(circ(.7));
+// //*arrays
+// let names = ["Brian", "Ron", "Robin", "Steven"];
+// names.push("Bubby");
+// // names.push(4);
+// // names.push(true);
+// // names[0] = false;
+// let numbers = [10, 23, 30, 40];
+// numbers.push(25);
+// // numbers.push('Hello');
+// // let me = 'Brian';
+// // numbers.push(me)
+// let mixed = ["Ken", 4, "string", 8, 9];
+// mixed.push(44);
+// mixed.push("Text");
+// // mixed.push(false)
+// // mixed.push(true)
+// mixed[0] = 0;
+// mixed[1] = "string";
+// //* objects. Property types become fixed
+// let ninja = {
+//   name: "Brian",
+//   belt: "black",
+//   age: 30,
+// };
+// ninja.age = 40;
+// ninja.name = "Test";
+// // ninja.age = "30";
+// // ninja.skills = ['fight', 'rest', 'sneaking']; //*new property cannot be added to existing object
+// ninja = {
+//   ...ninja,
+//   // skills: "test"
+// };
+// ninja = { //must match exact same shape
+//   name: "Patrick",
+//   belt: "blue",
+//   age: 32,
+//   // skills: []
+// };
+//!#5
+//!#5
+//!#5
+// //explicit types - not defining a value, but defining the type
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
+// // age = '30' //*fail, wrong type
+// // age = true //*fail, wrong type
+// age = 30;
+// //ARRAYS
+// let ninjas: string[]; //an array of strings. Undefined, no value initialized
+// let ninjasInit: string[] = []; //an array of strings. Here initialized and can be pushed onto.
+// // ninjas = [10, 21] //*fail, wrong type
+// ninjas = ["Brian", "Zander"];
+// //UNION TYPE - one or two or one of three types
+// let mixed: (string | number)[] = []; //* | is used for multiple types
+// mixed.push("test");
+// mixed.push(22);
+// // mixed.push(false) //*=> fail/linting error. because boolean isn't in the union ()
+// let mixed3: (string | number | boolean)[] = []; //* | is used for multiple types
+// mixed3.push("test");
+// mixed3.push(22);
+// mixed3.push(false); //*=> acceptable because boolean is not in the union ()
+// //Union type infront of an array needs (), but if just a varaible no () are needed
+// let uid: string | number;
+// uid = 123;
+// uid = "123";
+// // uid = true; //*error, since boolean was not added to union type definintion
+// //!OBJECT
+// let ninjaOne: object;
+// ninjaOne = { name: "Brian", age: 30 };
+// // ninjaOne = 'string' //*nope! This is an object
+// let ninjaTwo: {
+//   name: string;
+//   age: number;
+//   beltColor: string;
+//   isAboveGreenBelt: boolean;
+// }; //*this is NOT the same as... let ninjaTwo = {}. This is an object. And it must have these properties of these types
+// ninjaTwo = {
+//   name: 'Brian',
+//   age: 30,
+//   beltColor: 'yellow',
+//   isAboveGreenBelt: false,
+// }; //*more or less properties than defined originally doesn't pass.
+//!#6
+//!#6 - Dynamic (any) types
+//!#6
--- sandbox.ts
+++ sandbox.ts
@@ -7,69 +7,126 @@
 //*everytime you make a change in the .ts file you need to rerun tsc sandbox.ts
 //*... unless you run the command $ tsx sandbox.ts -w (w is for watch)
 
-const log = console.log;
+// const log = console.log;
 
-let character = "Brian";
-let age = 30;
-let isBlackBelt = false;
+// let character = "Brian";
+// let age = 30;
+// let isBlackBelt = false;
 
-// character = 20;
-character = "Ron";
+// // character = 20;
+// character = "Ron";
 
-// age = 'thirty'
-age = 40;
+// // age = 'thirty'
+// age = 40;
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+// // isBlackBelt = 'yes';
+// isBlackBelt = true;
 
-const circ = (diameter: number) => {
-  // const circ = (diameter: string) => {
-  // const circ = (diameter: boolean) => {
-  return diameter * Math.PI;
-};
-console.log(circ(7.5));
+// const circ = (diameter: number) => {
+//   // const circ = (diameter: string) => {
+//   // const circ = (diameter: boolean) => {
+//   return diameter * Math.PI;
+// };
+// // console.log(circ(.7));
 
-//*arrays
-let names = ["Brian", "Ron", "Robin", "Steven"];
+// //*arrays
+// let names = ["Brian", "Ron", "Robin", "Steven"];
 
-names.push("Bubby");
-// names.push(4);
-// names.push(true);
-// names[0] = false;
+// names.push("Bubby");
+// // names.push(4);
+// // names.push(true);
+// // names[0] = false;
 
-let numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('Hello');
-// let me = 'Brian';
-// numbers.push(me)
+// let numbers = [10, 23, 30, 40];
+// numbers.push(25);
+// // numbers.push('Hello');
+// // let me = 'Brian';
+// // numbers.push(me)
 
-let mixed = ["Ken", 4, "string", 8, 9];
-mixed.push(44);
-mixed.push("Text");
-// mixed.push(false)
-// mixed.push(true)
-mixed[0] = 0;
-mixed[1] = "string";
+// let mixed = ["Ken", 4, "string", 8, 9];
+// mixed.push(44);
+// mixed.push("Text");
+// // mixed.push(false)
+// // mixed.push(true)
+// mixed[0] = 0;
+// mixed[1] = "string";
 
-//* objects. Property types become fixed
-let ninja = {
-  name: "Brian",
-  belt: "black",
-  age: 30,
-};
-ninja.age = 40;
-ninja.name = "Test";
-// ninja.age = "30";
-// ninja.skills = ['fight', 'rest', 'sneaking']; //*new property cannot be added to existing object
+// //* objects. Property types become fixed
+// let ninja = {
+//   name: "Brian",
+//   belt: "black",
+//   age: 30,
+// };
+// ninja.age = 40;
+// ninja.name = "Test";
+// // ninja.age = "30";
+// // ninja.skills = ['fight', 'rest', 'sneaking']; //*new property cannot be added to existing object
 
-ninja = {
-  ...ninja,
-  // skills: "test"
-};
+// ninja = {
+//   ...ninja,
+//   // skills: "test"
+// };
 
-ninja = { //must match exact same shape
-  name: "Patrick",
-  belt: "blue",
-  age: 32,
-  // skills: []
-};
+// ninja = { //must match exact same shape
+//   name: "Patrick",
+//   belt: "blue",
+//   age: 32,
+//   // skills: []
+// };
+
+//!#5
+//!#5
+//!#5
+// //explicit types - not defining a value, but defining the type
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
+
+// // age = '30' //*fail, wrong type
+// // age = true //*fail, wrong type
+// age = 30;
+
+// //ARRAYS
+// let ninjas: string[]; //an array of strings. Undefined, no value initialized
+// let ninjasInit: string[] = []; //an array of strings. Here initialized and can be pushed onto.
+// // ninjas = [10, 21] //*fail, wrong type
+// ninjas = ["Brian", "Zander"];
+
+// //UNION TYPE - one or two or one of three types
+// let mixed: (string | number)[] = []; //* | is used for multiple types
+// mixed.push("test");
+// mixed.push(22);
+// // mixed.push(false) //*=> fail/linting error. because boolean isn't in the union ()
+
+// let mixed3: (string | number | boolean)[] = []; //* | is used for multiple types
+// mixed3.push("test");
+// mixed3.push(22);
+// mixed3.push(false); //*=> acceptable because boolean is not in the union ()
+
+// //Union type infront of an array needs (), but if just a varaible no () are needed
+// let uid: string | number;
+// uid = 123;
+// uid = "123";
+// // uid = true; //*error, since boolean was not added to union type definintion
+
+// //!OBJECT
+// let ninjaOne: object;
+// ninjaOne = { name: "Brian", age: 30 };
+// // ninjaOne = 'string' //*nope! This is an object
+
+// let ninjaTwo: {
+//   name: string;
+//   age: number;
+//   beltColor: string;
+//   isAboveGreenBelt: boolean;
+// }; //*this is NOT the same as... let ninjaTwo = {}. This is an object. And it must have these properties of these types
+// ninjaTwo = {
+//   name: 'Brian',
+//   age: 30,
+//   beltColor: 'yellow',
+//   isAboveGreenBelt: false,
+// }; //*more or less properties than defined originally doesn't pass.
+
+//!#6
+//!#6 - Dynamic (any) types
+//!#6
